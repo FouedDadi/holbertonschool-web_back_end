dbf6448e1be597dc70f6ea3bb0b2e85c16af1911
FouedDadi <foued.dadii@gmail.com>
2021-03-15 20:50:57 +0100

    take a float n and return the string representation of that float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""[take a float n and return the string representation of that float]"""
+
+
+def to_str(n: float) -> str:
+    """[summary]
+
+    Args:
+        n (float): [argument]
+
+    Returns:
+        str: [string representation of the float n]
+    """
+    return str(n)
