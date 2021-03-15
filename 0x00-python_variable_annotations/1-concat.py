42c47824443ab1d8d74f59a3bc52bc9e967c7f49
FouedDadi <foued.dadii@gmail.com>
2021-03-15 20:43:54 +0100

    sum two strings arguments and return the concatination as a string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""[sum two strings arguments and return the result as string]"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """[a type-annotated function]
+
+    Args:
+        str1 (str): [str argument 1]
+        str2 (str): [str argument 2]
+
+    Returns:
+        str: [concatination of str1 and str2]
+    """
+    return str1 + str2
