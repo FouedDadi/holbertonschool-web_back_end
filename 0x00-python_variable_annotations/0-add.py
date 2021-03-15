bcfa94c494423a96c96a84d03e4ca9fcad2f0667
FouedDadi <foued.dadii@gmail.com>
2021-03-15 20:37:06 +0100

    sum of two float arguments and return the result as float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""[sum two float arguments and return the result as float ]"""
+
+
+def add(a: float, b: float) -> float:
+    """[a type-annotated function ]
+
+    Args:
+        a (float): [argument]
+        b (float): [argument]
+
+    Returns:
+        float: [the sum of a and b arguments]
+    """
+    return a + b
