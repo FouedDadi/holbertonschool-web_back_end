8c52fe3f96d2f689d068e9b7595eac5b19d99210
FouedDadi <foued.dadii@gmail.com>
2021-03-15 20:47:41 +0100

    take a float n and return the floor of that float (int)

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""[take a float n and return the floor of that float (int)]"""
+
+
+def floor(n: float) -> int:
+    """[summary]
+
+    Args:
+        n (float): [argument]
+
+    Returns:
+        int: [floor of the float n]
+    """
+    return int(n)
