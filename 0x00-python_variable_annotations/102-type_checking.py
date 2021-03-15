cf1e2f9c76410accedf36769af46ac4776ef94b1
FouedDadi <foued.dadii@gmail.com>
2021-03-15 22:12:10 +0100

    annotate a function

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""[annotate a function]"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """[duck-typing]
+
+    Args:
+        lst (Tuple): [arg 1]
+        factor (int, optional): [arg 2]. Defaults to 2.
+
+    Returns:
+        List: []
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(tuple(array))
+
+zoom_3x = zoom_array(tuple(array), int(3.0))
