977a79f307d63f414cc1b14b5c8692a5cc33db23
FouedDadi <foued.dadii@gmail.com>
2021-03-16 18:07:12 +0100

    asynchronous coroutine that takes in an int and awaits for random delay time

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -13,6 +13,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Returns:
         float: [random await]
     """
-    randomised: float = random.uniform(0, max_delay)
+    randomised = random.uniform(0, max_delay)
     await asyncio.sleep(randomised)
     return randomised
