f9f59a10fc6a10cd4e3680b5c2da98b281e0c7b3
FouedDadi <foued.dadii@gmail.com>
2021-03-17 21:22:26 +0100

    loop 10 times then yield a random number between 0 and 10

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3"
-"""[loop 10 times yield a random number between 0 and 10.]"""
+"""[loop 10 times then yield a random number between 0 and 10]"""
 import asyncio
 import random
 from typing import Generator
@@ -11,6 +11,6 @@ async def async_generator() -> Generator[float, None, None]:
     Yields:
         Generator[float, None, None]: []
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.random(0, 10)
