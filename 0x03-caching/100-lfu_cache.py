a7e20c5d337604f8fd4ce38b88fb429dd9a9a0e5
FouedDadi <foued.dadii@gmail.com>
2021-03-22 01:24:41 +0100

    Create a class LFUCache that inherits from BaseCaching

---
+++ 0x03-caching/100-lfu_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Create a class LFUCache that inherits from BaseCaching"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """[class LFUCache]
+
+    Args:
+        BaseCaching ([type]): [description]
+    """
+    _cache = []
+
+    def put(self, key, item):
+        """[put method]
+
+        Args:
+            key ([type]): [description]
+            item ([type]): [description]
+        """
+
+        if key and item:
+            self.cache_data[key] = item
+            if key not in self._cache:
+                self._cache.append(key)
+            else:
+                self._cache.append(self._cache.pop(self._cache.index(key)))
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                deleted = self._cache.pop(0)
+                del self.cache_data[deleted]
+                print("DISCARD: {}".format(deleted))
+
+    def get(self, key):
+        """[get method]
+
+        Args:
+            key ([type]): [description]
+        """
+        if key and self.cache_data.get(key):
+            self._cache.append(self._cache.pop(self._cache.index(key)))
+            return self.cache_data.get(key)
