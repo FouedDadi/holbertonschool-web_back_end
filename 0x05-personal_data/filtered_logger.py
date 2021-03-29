c0e6b55a57955f88255ae51aba61a3c5e5392999
FouedDadi <foued.dadii@gmail.com>
2021-03-29 16:26:17 +0100

    updating the class

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,37 @@
 """[contains different function]"""
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """[initialisation]
+
+        Args:
+            fields (List[str]): [description]
+        """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """[updating format]
+
+        Args:
+            record (logging.LogRecord): [description]
+
+        Returns:
+            str: [description]
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,
