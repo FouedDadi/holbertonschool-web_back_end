98b1f8f2027b071baa5f105d65c88f2c487d580d
FouedDadi <foued.dadii@gmail.com>
2021-03-29 16:34:54 +0100

    updating the class

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -19,8 +19,8 @@ class RedactingFormatter(logging.Formatter):
         Args:
             fields (List[str]): [description]
         """
-        self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """[updating format]
@@ -52,3 +52,11 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(rf'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+def get_logger() -> logging.Logger:
+    """[summary]
+
+    Returns:
+        logging.Logger: [description]
+    """
+    
