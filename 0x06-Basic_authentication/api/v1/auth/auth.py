514d1952ef76a34448a71c39e6c29871dd424c13
FouedDadi <foued.dadii@gmail.com>
2021-03-31 15:13:23 +0100

    adding conditions

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -16,7 +16,15 @@ class Auth:
         Returns:
             bool: [description]
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """[summary]
