1dee4cbb9954b9122924396c4af33ba04fb4fcf4
FouedDadi <foued.dadii@gmail.com>
2021-03-31 20:11:17 +0100

    validate all requests to secure the API

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -35,7 +35,11 @@ class Auth:
         Returns:
             str: [description]
         """
-        return None
+        if request is None:
+            return None
+        if "Authorization" not in request.headers:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """[summary]
