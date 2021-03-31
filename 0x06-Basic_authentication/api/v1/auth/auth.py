c0c46480ff828b1af62abeb66ddc2f6aee243d1c
FouedDadi <foued.dadii@gmail.com>
2021-03-31 14:47:02 +0100

    auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -5,9 +5,32 @@ from typing import TypeVar, List
 
 
 class Auth:
+    """[summary]
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """[summary]
+        Args:
+            path (str): [description]
+            excluded_paths (List[str]): [description]
+
+        Returns:
+            bool: [description]
+        """
         return False
     def authorization_header(self, request=None) -> str:
+        """[summary]
+
+        Args:
+            request ([type], optional): [description]. Defaults to None.
+
+        Returns:
+            str: [description]
+        """
         return None
     def current_user(self, request=None) -> TypeVar('User'):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
         return None
