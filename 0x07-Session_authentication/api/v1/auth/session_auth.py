59af5924dfeda9602fee5577b69cdfcf379f79a8
FouedDadi <foued.dadii@gmail.com>
2021-04-06 18:37:25 +0100

    adding method create_session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """[summary]"""
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -10,3 +11,20 @@ class SessionAuth(Auth):
     Args:
         Auth ([type]): [description]
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """[summary]
+
+        Args:
+            user_id (str, optional): [description]. Defaults to None.
+
+        Returns:
+            str: [description]
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        else:
+            sess_id = str(uuid.uuid4())
+            self.user_id_by_session_id[sess_id] = user_id
+            return sess_id
