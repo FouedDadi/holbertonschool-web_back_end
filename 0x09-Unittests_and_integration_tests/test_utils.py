a4294d810197792964d3d65de957c831fc4b0928
FouedDadi <foued.dadii@gmail.com>
2021-04-19 18:49:11 +0100

    adding file test_utils

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""[summary]"""
+import unittest
+from mock import patch, MagicMock
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """[summary]
+
+    Args:
+        unittest ([type]): [description]
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """[summary]
+
+        Args:
+            map ([type]): [description]
+            path ([type]): [description]
+            output ([type]): [description]
+        """
+        output = access_nested_map(map, path)
+        self.assertEqual(output, expected)
