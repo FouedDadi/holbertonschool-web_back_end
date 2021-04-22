1acf84bd1ad6710e346293b05b1b074e07c8620c
FouedDadi <foued.dadii@gmail.com>
2021-04-22 17:42:11 +0100

    babel configuration

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""[flask app to render a template]"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """[summary]
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object('1-app.config')
+
+
+@app.route('/')
+def acceuil():
+    """[summary]
+    """
+    return render_template('1-index.html')
