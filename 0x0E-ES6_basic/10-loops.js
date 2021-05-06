27a0df629f90fd1c4a253723c6ad98a524ff6166
FouedDadi <foued.dadii@gmail.com>
2021-05-06 23:51:08 +0100

    changing to const

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr = [];
-  for (let val of array) {
+  for (const val of array) {
     arr.push(appendString + val);
   }
   return arr;
