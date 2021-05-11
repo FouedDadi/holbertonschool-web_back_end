3a852ff2177fcea6346a5a9d65e32c041ddfedde
FouedDadi <foued.dadii@gmail.com>
2021-05-11 23:18:43 +0100

    pushing different things to queue

--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -3,9 +3,10 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (error) {
-    queue.push(error.toSting());
+    queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
+
   return queue;
 }
