263a76e09d8b4b957e87ab5532b1e33e1dd0d6c4
FouedDadi <foued.dadii@gmail.com>
2021-05-11 23:30:25 +0100

    resolve all promises and log body firstName lastName to the console

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,19 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let body;
-  let firstName;
-  let lastName;
   return uploadPhoto()
-    .then((dt) => {
-      body = dt.body;
-      createUser()
-        .then((dt) => {
-          firstName = dt.firstName;
-          lastName = dt.lastName;
-          console.log(`${body} ${firstName} ${lastName}`);
-        })
-        .catch(() => console.log('Signup system offline'));
+    .then((body) => {
+      createUser().then(({ firstName, lastName }) => {
+        console.log(`${body} ${firstName} ${lastName}`);
+      });
     })
     .catch(() => console.log('Signup system offline'));
 }
