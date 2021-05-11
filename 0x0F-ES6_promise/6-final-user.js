ca9b40ea941dc281ce0daf784cc744c45857a980
FouedDadi <foued.dadii@gmail.com>
2021-05-11 23:06:33 +0100

    failed to finish this task

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-all-reject';
+import uploadPhoto from './5-all-reject';
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+
+}
