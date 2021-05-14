e06555101b575bb698ea6bcd39e6744ab5ccab67
FouedDadi <foued.dadii@gmail.com>
2021-05-14 15:10:04 +0100

    fixing task 2

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,10 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw new TypeError('name must be a string');
-    if (typeof length !== 'number')
-      throw new TypeError('Length must be a number');
-    if (!Array.isArray(students))
-      throw new TypeError('students must be an array');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students must be an array');
     for (const std of students) {
       if (typeof std !== 'string') throw new TypeError('std must be a string');
     }
@@ -19,14 +17,12 @@ export default class HolbertonCourse {
   }
 
   set length(Length) {
-    if (typeof Length !== 'number')
-      throw new TypeError('Length must be a number');
+    if (typeof Length !== 'number') throw new TypeError('Length must be a number');
     this._length = Length;
   }
 
   set students(Students) {
-    if (!Array.isArray(Students))
-      throw new TypeError('Stduents must be an array');
+    if (!Array.isArray(Students)) throw new TypeError('Stduents must be an array');
     for (const std of Students) {
       if (typeof std !== 'string') throw new TypeError('std must be a string');
     }
