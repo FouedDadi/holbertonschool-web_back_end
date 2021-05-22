8395d82ffd9c817c7e524ec8b9b6adc1459cd310
FouedDadi <foued.dadii@gmail.com>
2021-05-22 16:57:03 +0100

    program to take input and print it

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -3,8 +3,6 @@ process.stdin.once('data', (chunk) => {
   const name = chunk.toString().trim();
   if (name) {
     console.log(`Your name is: ${name}`);
+    process.exit();
   }
-  process.stdin.on('close', () => {
-    console.log('This important software is now closing');
-  });
 });
