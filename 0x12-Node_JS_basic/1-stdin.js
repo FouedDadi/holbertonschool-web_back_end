cbfbef816c5511493c6789b61d10bd5262c931d2
FouedDadi <foued.dadii@gmail.com>
2021-05-22 16:47:46 +0100

    program to take input and print it

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.once('data', (chunk) => {
+  const name = chunk.toString().trim();
+  if (name) {
+    console.log(`Your name is: ${name}`);
+  }
+  process.stdin.on('close', () => {
+    console.log('This important software is now closing');
+  });
+});
