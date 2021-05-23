f51720d77ac3ce157100b0aad20a195d54b095b8
FouedDadi <foued.dadii@gmail.com>
2021-05-23 15:32:45 +0100

    creating http server using http module

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+// create a server object:
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' }); // HTTP header with content type
+  res.write('Hello Holberton School!'); // write a response to the client
+  res.end(); // end the response
+});
+app.listen(1245);
