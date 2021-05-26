68ff3efc12de96e46a5c0d2e321bb5f244bc40d9
FouedDadi <foued.dadii@gmail.com>
2021-05-26 17:07:19 +0100

    fixing test file

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,16 +1,22 @@
-const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
 
-describe('smoke test', function () {
-  it('checks output and negative args', function () {
+describe('calculateNumber', () => {
+  it('checks the output', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(-2, 4), 2);
-    assert.strictEqual(calculateNumber(-5, -10), -15);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
   });
-  it('check arguments', function () {
-    assert.strictEqual(isNaN(calculateNumber(2)), true);
+  it('negative numbers', () => {
+    assert.strictEqual(calculateNumber(-1, 1), 0);
+    assert.strictEqual(calculateNumber(1, -1), 0);
+    assert.strictEqual(calculateNumber(-1, -1), -2);
+  });
+  it('checks arguments', () => {
+    assert.strictEqual(isNaN(calculateNumber(1)), true);
+    assert.strictEqual(isNaN(calculateNumber()), true);
   });
 });
