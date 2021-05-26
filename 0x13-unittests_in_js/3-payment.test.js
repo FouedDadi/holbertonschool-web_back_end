8c8577cee034059e4f7ba3bff2a41b509db3f62a
FouedDadi <foued.dadii@gmail.com>
2021-05-26 19:23:07 +0100

    adding files for payment function and unittests

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -7,7 +7,8 @@ describe('smoke test', function () {
   const spies = sinon.spy(utl, 'calculateNumber');
   it('checks output', function () {
     sendPaymentRequestToApi(100, 20);
-    chai.expect(spies.calledWith('SUM', 100, 20)).to.equal(true);
+    chai.expect(spies.calledOnce).to.be.true;
+    chai.expect(spies.calledWith('SUM', 100, 20)).to.be.true;
     spies.restore();
   });
 });
