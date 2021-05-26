47620bf6566516ee8ebc93f63c0a77ca7d93b129
FouedDadi <foued.dadii@gmail.com>
2021-05-26 17:09:28 +0100

    fixing test file

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,16 +1,18 @@
 const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
-describe('smoke test', function () {
-  it('checks output and negative args', function () {
+describe('calculateNumber', function () {
+  it('checks output', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(-2, 4), 2);
-    assert.strictEqual(calculateNumber(-5, -10), -15);
   });
   it('check arguments', function () {
     assert.strictEqual(isNaN(calculateNumber(2)), true);
+  });
+  it('check negative args', function () {
+    assert.strictEqual(calculateNumber(-2, 4), 2);
+    assert.strictEqual(calculateNumber(-5, -10), -15);
   });
 });
