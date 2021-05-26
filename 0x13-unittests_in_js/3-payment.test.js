5a14f09367e1ef8f7c4dbdaca00cafb7e0f9231b
FouedDadi <foued.dadii@gmail.com>
2021-05-26 19:34:24 +0100

    adding files for payment function and unittests

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -1,12 +1,12 @@
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment.js');
-const utl = require('./utils.js');
+const utl = require('./utils');
 const chai = require('chai');
 
 describe('smoke test', function () {
   it('checks output', function () {
     const spies = sinon.spy(utl, 'calculateNumber');
-    sendPaymentRequestToApi(100, 20);
+    const api = sendPaymentRequestToApi(100, 20);
     chai.expect(spies.calledWith('SUM', 100, 20)).to.equal(true);
     spies.restore();
   });
