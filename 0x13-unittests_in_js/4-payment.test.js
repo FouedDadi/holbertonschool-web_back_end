415af8a1852263128e56b1c51fcc2c8cc384a133
FouedDadi <foued.dadii@gmail.com>
2021-05-26 19:44:31 +0100

    adding files for task 4

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -6,8 +6,7 @@ const chai = require('chai');
 describe('smoke test', function () {
   it('checks output', function () {
     const con = sinon.spy(console, 'log');
-    const stb = sinon.stub(utl, 'calculateNumber');
-    stb.returns(10);
+    const stb = sinon.stub(utl, 'calculateNumber').returns(10);
     const api = sendPaymentRequestToApi(100, 20);
     chai.expect(con.calledWith('The total is: 10')).to.equal(true);
     chai.expect(stb.calledWith('SUM', 100, 20)).to.equal(true);
